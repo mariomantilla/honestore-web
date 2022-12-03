fd77d7415102d2efbcbbe30dff78d205a608fbde
Mario Mantilla <mario@solarisoffgrid.com>
2022-12-03 21:42:09 +0100

    First commit

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
+import { Analytics } from '@vercel/analytics/react';
+
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session,
+}>) {
+  const [supabase] = useState(() => createBrowserSupabaseClient())
+
+  return (
+    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+      <Component {...pageProps} />
+      <Analytics />
+    </SessionContextProvider>
+  )
+}
+export default MyApp
