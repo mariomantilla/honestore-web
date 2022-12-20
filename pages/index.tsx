0a26bf507e278bdecbe544c3a445dba8b8741fd1
Mario Mantilla <mario@solarisoffgrid.com>
2022-12-20 21:38:36 +0100

    Added newsletter

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,10 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import Underline from '../components/underline';
+import React from 'react';
+import Send from '@mui/icons-material/Send';
+import Snackbar from '@mui/material/Snackbar';
+import Divider from '@mui/material/Divider';
 
 
 async function getShops(supabase: SupabaseClient, searchQuery: string): Promise<Shop[] | null> {
@@ -33,10 +36,36 @@ export default function Home() {
 
   const supabase = useSupabaseClient();
 
+  const [open, setOpen] = React.useState(false);
+  const [email, setEmail] = React.useState('');
+  const [snackBarMessage, setsnackBarMessage] = React.useState('');
+
+  const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
   useEffect(() => {
     getShops(supabase, searchQuery).then((shopsData) => setShops(shopsData));
   }, [searchQuery, supabase]);
 
+  async function addContact() {
+    const response = await fetch("/api/addContact", {
+      method: "POST",
+      body: JSON.stringify({ email: email }),
+    });
+    if (response.status == 200) {
+      setsnackBarMessage('Te has suscrito correctamente, por favor verifica tu email');
+      setOpen(true);
+    } else {
+      setsnackBarMessage('Ha ocurrido un error');
+      setOpen(true);
+    }
+  }
+
+  const SendButton = <Button onClick={addContact}><Send /></Button>
 
 
   return (
@@ -44,13 +73,15 @@ export default function Home() {
       <Typography variant='h2' component="h1">La comunidad de activistas del consumo ético</Typography>
       <Grid container spacing={2}>
         <Grid item xs={12} lg={3}>
-          {/* <Typography variant='h4' component="div" sx={{marginBottom: 1.3}}><Underline>Sobre Honestore</Underline></Typography> */}
-          <Typography component="h2" variant="subtitle1" sx={{marginBottom: 1.3}}>
-          Somos una red de personas y tiendas unidas por cambiar hacia un consumo consciente y basado en valores sociales y medioambientales.
+          <Typography component="h2" variant="subtitle1" sx={{ marginBottom: 1.3 }}>
+            Somos una red de personas y tiendas unidas por cambiar hacia un consumo consciente y basado en valores sociales y medioambientales.
           </Typography>
-          <Box sx={{display: "flex", justifyContent: "center"}}>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
             <Button variant='contained'><Link href="/about">Saber más</Link></Button>
           </Box>
+          <TextField id="outlined-basic" onChange={(e) => setEmail(e.target.value)} fullWidth placeholder="Suscribete al newsletter" size="small" variant="outlined" sx={{ marginTop: 2 }} InputProps={{ endAdornment: SendButton }} />
+          <Typography variant="caption">He leído y acepto la <Link href="/privacy">Política de Privaciad</Link></Typography>
+          <Divider sx={{marginTop: 1.5, display: {lg: "none"}}} />
         </Grid>
         <Grid item xs={12} lg={9}>
           <TextField id="outlined-basic" fullWidth placeholder="Buscar tiendas y emprendimientos" variant="outlined" size="small" onChange={(v) => { setSearchQuery(v.target.value) }} />
@@ -62,6 +93,12 @@ export default function Home() {
           <AddIcon sx={{ color: "white" }} />
         </Link>
       </Fab>
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message={snackBarMessage}
+      />
     </>
   )
 }
