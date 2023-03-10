49af2bf369bde4b15a9dc8936401467a7c928601
Mario Mantilla <mario@solarisoffgrid.com>
2023-03-10 01:09:43 +0100

    Fixed newsletter form

--- components/addContact.tsx
+++ components/addContact.tsx
@@ -3,32 +3,35 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import Link from "next/link";
+import { useState } from "react";
 import { useMessagesContext } from "../context/messages";
-
-
 
 
 export default function AddContact() {
 
     const { sendMessage } = useMessagesContext();
+    const [email, setEmail] = useState('');
 
-    async function addContact(e) {
-        console.log(e);
-        // const response = await fetch("/api/addContact", {
-        //     method: "POST",
-        //     body: JSON.stringify({ email: email }),
-        // });
-        // if (response.status == 200) {
-        //     sendMessage('success', 'Te has suscrito correctamente, por favor verifica tu email');
-        // } else {
-        //     sendMessage('error', 'Ha ocurrido un error');
-        // }
+    async function addContact() {
+        const response = await fetch("/api/addContact", {
+            method: "POST",
+            body: JSON.stringify({ email: email }),
+        });
+        if (response.status == 200) {
+            sendMessage('success', 'Te has suscrito correctamente, por favor verifica tu email');
+            setEmail('');
+        } else if (response.status == 400) {
+            let json = await response.json();
+            sendMessage('error', json.message.message);
+        } else {
+            sendMessage('error', 'Ha ocurrido un error');
+        }
     }
 
     return (
         <>
             <TextField
-                onChange={(e) => ''/*setEmail(e.target.value)*/}
+                onChange={(e) => setEmail(e.target.value)}
                 fullWidth
                 placeholder="Suscribete al newsletter"
                 size="small"
