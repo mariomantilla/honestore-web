aa59c977838c280996335aa860a6b9a926a8f2c2
Mario Mantilla <mario@solarisoffgrid.com>
2023-03-13 23:00:19 +0100

    fixed type error

--- pages/shops/[id].tsx
+++ pages/shops/[id].tsx
@@ -71,103 +71,103 @@ export async function getStaticProps({ params }: { params: { id: number } }) {
 
 export default function ShopPage({ shop }: { shop: Shop }) {
 
-  return <></>
+  return <></>;
 
-  const router = useRouter()
-  if (router.isFallback) {
-    return <Container sx={{ textAlign: "center" }}><CircularProgress /></Container>
-  }
+  // const router = useRouter()
+  // if (router.isFallback) {
+  //   return <Container sx={{ textAlign: "center" }}><CircularProgress /></Container>
+  // }
 
-  const logoUrl = DataService.getShopLogo(shop);
+  // const logoUrl = DataService.getShopLogo(shop);
 
-  let actions: React.ReactNode[] = [];
+  // let actions: React.ReactNode[] = [];
 
-  if (shop.instagram) {
-    let url: string = `https://instagram.com/${shop.instagram}`;
-    actions.push(
-      <ShopExternalAction title="Ir a Instagram" url={url} key="instagram"><Instagram color='primary' /></ShopExternalAction>
-    )
-  }
-  if (shop.phone) {
-    let url: string = `tel:${shop.phone}`;
-    actions.push(
-      <ShopExternalAction title="Llamar" url={url} key="phone"><Phone color='primary' /></ShopExternalAction>
-    )
-  }
-  if (shop.web) {
-    actions.push(
-      <ShopExternalAction title="Ir a la web" url={shop.web} key="web"><Language color='primary' /></ShopExternalAction>
-    )
-  }
-  if (shop.email) {
-    let url: string = `mailto:${shop.email}`;
-    actions.push(
-      <ShopExternalAction title="Escribir email" url={url} key="email"><Email color='primary' /></ShopExternalAction>
-    )
-  }
-  if (shop.location_coordinates && !shop.online) {
-    let url: string = `https://www.google.com/maps/search/?api=1&query=${shop.location_coordinates.replace(' ', ',')}`;
-    actions.push(
-      <ShopExternalAction title="Abrir mapa" url={url} key="map"><Map color='primary' /></ShopExternalAction>
-    )
-  }
+  // if (shop.instagram) {
+  //   let url: string = `https://instagram.com/${shop.instagram}`;
+  //   actions.push(
+  //     <ShopExternalAction title="Ir a Instagram" url={url} key="instagram"><Instagram color='primary' /></ShopExternalAction>
+  //   )
+  // }
+  // if (shop.phone) {
+  //   let url: string = `tel:${shop.phone}`;
+  //   actions.push(
+  //     <ShopExternalAction title="Llamar" url={url} key="phone"><Phone color='primary' /></ShopExternalAction>
+  //   )
+  // }
+  // if (shop.web) {
+  //   actions.push(
+  //     <ShopExternalAction title="Ir a la web" url={shop.web} key="web"><Language color='primary' /></ShopExternalAction>
+  //   )
+  // }
+  // if (shop.email) {
+  //   let url: string = `mailto:${shop.email}`;
+  //   actions.push(
+  //     <ShopExternalAction title="Escribir email" url={url} key="email"><Email color='primary' /></ShopExternalAction>
+  //   )
+  // }
+  // if (shop.location_coordinates && !shop.online) {
+  //   let url: string = `https://www.google.com/maps/search/?api=1&query=${shop.location_coordinates.replace(' ', ',')}`;
+  //   actions.push(
+  //     <ShopExternalAction title="Abrir mapa" url={url} key="map"><Map color='primary' /></ShopExternalAction>
+  //   )
+  // }
 
-  const onlineChip = shop && shop.online ? (
-    <Chip icon={<LocationOff />} label="Solo online" />
-  ) : '';
+  // const onlineChip = shop && shop.online ? (
+  //   <Chip icon={<LocationOff />} label="Solo online" />
+  // ) : '';
 
-  return (
-    <>
-      <Head>
-        <title>{shop.name + " en Honestore"}</title>
-        <meta name="description" content={shop.description ?? ''} />
-        <meta property="og:title" content={shop.name + " en Honestore"} />
-        <meta property="og:description" content={shop.description ?? ''} />
-      </Head>
-      <Grid container spacing={1.5} rowSpacing={3}>
-        <Grid xs={12} sm={6} md={8} sx={{position: "relative"}}>
-          <Box sx={{position: "absolute", top: 0, right: "1rem"}}>
-            <FavButton shop={shop} size="large" />
-          </Box>
-          <Box sx={{ display: "flex", gap: 2, flexDirection: "column" }}>
-            <Box sx={{ display: "flex", gap: 1, alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
-              <Avatar alt={shop.name ?? ''} src={logoUrl} sx={{ height: 128, width: 128, border: "1px solid #ccc" }} />
-              <Typography variant="h1" component="h1" sx={{ marginBottom: 0 }}>{shop.name}</Typography>
-              <Typography variant="body2" color="text.secondary" fontWeight="bold">{shop.address}</Typography>
-              {onlineChip}
-            </Box>
-            <Box sx={{ display: "flex", gap: 3, justifyContent: "center" }}>
-              {actions}
-            </Box>
-            <Divider />
-            <Box sx={{ display: "flex", justifyContent: "center" }}>
-              <Typography sx={{ whiteSpace: "pre-wrap" }}>{shop.description}</Typography>
-            </Box>
-          </Box>
-        </Grid>
-        <Grid xs={12} sm={6} md={4}>
-          <Card elevation={4} sx={{ borderRadius: "10px" }}>
-            <CardContent sx={{ textAlign: "center" }}>
-              <Image
-                src={banner}
-                width={300}
-                priority
-                alt="Honestore, La comunidad de activistas del consumo ético" style={{ maxWidth: "100%", height: "auto" }}
-              />
-              <Typography sx={{ padding: "0.5rem 0.8rem", fontWeight: "bold" }}>La comunidad de activistas del consumo ético</Typography>
-              <Button href="/about" LinkComponent={Link} variant="contained">
-                Descubrir más
-              </Button>
-              <Divider sx={{margin: "1rem 0"}} />
-              <Button href="/search" LinkComponent={Link} variant="contained">
-                Buscar más tiendas
-              </Button>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
-      <Divider sx={{ marginTop: "1rem" }} />
-      <NewShops sx={{ marginTop: "1rem" }} />
-    </>
-  );
+  // return (
+  //   <>
+  //     <Head>
+  //       <title>{shop.name + " en Honestore"}</title>
+  //       <meta name="description" content={shop.description ?? ''} />
+  //       <meta property="og:title" content={shop.name + " en Honestore"} />
+  //       <meta property="og:description" content={shop.description ?? ''} />
+  //     </Head>
+  //     <Grid container spacing={1.5} rowSpacing={3}>
+  //       <Grid xs={12} sm={6} md={8} sx={{position: "relative"}}>
+  //         <Box sx={{position: "absolute", top: 0, right: "1rem"}}>
+  //           <FavButton shop={shop} size="large" />
+  //         </Box>
+  //         <Box sx={{ display: "flex", gap: 2, flexDirection: "column" }}>
+  //           <Box sx={{ display: "flex", gap: 1, alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
+  //             <Avatar alt={shop.name ?? ''} src={logoUrl} sx={{ height: 128, width: 128, border: "1px solid #ccc" }} />
+  //             <Typography variant="h1" component="h1" sx={{ marginBottom: 0 }}>{shop.name}</Typography>
+  //             <Typography variant="body2" color="text.secondary" fontWeight="bold">{shop.address}</Typography>
+  //             {onlineChip}
+  //           </Box>
+  //           <Box sx={{ display: "flex", gap: 3, justifyContent: "center" }}>
+  //             {actions}
+  //           </Box>
+  //           <Divider />
+  //           <Box sx={{ display: "flex", justifyContent: "center" }}>
+  //             <Typography sx={{ whiteSpace: "pre-wrap" }}>{shop.description}</Typography>
+  //           </Box>
+  //         </Box>
+  //       </Grid>
+  //       <Grid xs={12} sm={6} md={4}>
+  //         <Card elevation={4} sx={{ borderRadius: "10px" }}>
+  //           <CardContent sx={{ textAlign: "center" }}>
+  //             <Image
+  //               src={banner}
+  //               width={300}
+  //               priority
+  //               alt="Honestore, La comunidad de activistas del consumo ético" style={{ maxWidth: "100%", height: "auto" }}
+  //             />
+  //             <Typography sx={{ padding: "0.5rem 0.8rem", fontWeight: "bold" }}>La comunidad de activistas del consumo ético</Typography>
+  //             <Button href="/about" LinkComponent={Link} variant="contained">
+  //               Descubrir más
+  //             </Button>
+  //             <Divider sx={{margin: "1rem 0"}} />
+  //             <Button href="/search" LinkComponent={Link} variant="contained">
+  //               Buscar más tiendas
+  //             </Button>
+  //           </CardContent>
+  //         </Card>
+  //       </Grid>
+  //     </Grid>
+  //     <Divider sx={{ marginTop: "1rem" }} />
+  //     <NewShops sx={{ marginTop: "1rem" }} />
+  //   </>
+  // );
 }
