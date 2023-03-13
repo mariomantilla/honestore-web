935361bbd16562afee54191d51ae05b5daa61a49
Mario Mantilla <mario@solarisoffgrid.com>
2023-03-13 23:07:22 +0100

    Test

--- pages/shops/[id].tsx
+++ pages/shops/[id].tsx
@@ -70,51 +70,56 @@ export async function getStaticProps({ params }: { params: { id: number } }) {
 }
 
 export default function ShopPage({ shop }: { shop: Shop }) {
-
+  
   const router = useRouter()
   if (router.isFallback) {
     return <Container sx={{ textAlign: "center" }}><CircularProgress /></Container>
   }
 
-  const logoUrl = DataService.getShopLogo(shop);
-
-  let actions: React.ReactNode[] = [];
-
-  if (shop.instagram) {
-    let url: string = `https://instagram.com/${shop.instagram}`;
-    actions.push(
-      <ShopExternalAction title="Ir a Instagram" url={url} key="instagram"><Instagram color='primary' /></ShopExternalAction>
-    )
-  }
-  if (shop.phone) {
-    let url: string = `tel:${shop.phone}`;
-    actions.push(
-      <ShopExternalAction title="Llamar" url={url} key="phone"><Phone color='primary' /></ShopExternalAction>
-    )
-  }
-  if (shop.web) {
-    actions.push(
-      <ShopExternalAction title="Ir a la web" url={shop.web} key="web"><Language color='primary' /></ShopExternalAction>
-    )
-  }
-  if (shop.email) {
-    let url: string = `mailto:${shop.email}`;
-    actions.push(
-      <ShopExternalAction title="Escribir email" url={url} key="email"><Email color='primary' /></ShopExternalAction>
-    )
-  }
-  if (shop.location_coordinates && !shop.online) {
-    let url: string = `https://www.google.com/maps/search/?api=1&query=${shop.location_coordinates.replace(' ', ',')}`;
-    actions.push(
-      <ShopExternalAction title="Abrir mapa" url={url} key="map"><Map color='primary' /></ShopExternalAction>
-    )
-  }
-
-  const onlineChip = shop && shop.online ? (
-    <Chip icon={<LocationOff />} label="Solo online" />
-  ) : '';
-
   return <></>;
+
+  // const router = useRouter()
+  // if (router.isFallback) {
+  //   return <Container sx={{ textAlign: "center" }}><CircularProgress /></Container>
+  // }
+
+  // const logoUrl = DataService.getShopLogo(shop);
+
+  // let actions: React.ReactNode[] = [];
+
+  // if (shop.instagram) {
+  //   let url: string = `https://instagram.com/${shop.instagram}`;
+  //   actions.push(
+  //     <ShopExternalAction title="Ir a Instagram" url={url} key="instagram"><Instagram color='primary' /></ShopExternalAction>
+  //   )
+  // }
+  // if (shop.phone) {
+  //   let url: string = `tel:${shop.phone}`;
+  //   actions.push(
+  //     <ShopExternalAction title="Llamar" url={url} key="phone"><Phone color='primary' /></ShopExternalAction>
+  //   )
+  // }
+  // if (shop.web) {
+  //   actions.push(
+  //     <ShopExternalAction title="Ir a la web" url={shop.web} key="web"><Language color='primary' /></ShopExternalAction>
+  //   )
+  // }
+  // if (shop.email) {
+  //   let url: string = `mailto:${shop.email}`;
+  //   actions.push(
+  //     <ShopExternalAction title="Escribir email" url={url} key="email"><Email color='primary' /></ShopExternalAction>
+  //   )
+  // }
+  // if (shop.location_coordinates && !shop.online) {
+  //   let url: string = `https://www.google.com/maps/search/?api=1&query=${shop.location_coordinates.replace(' ', ',')}`;
+  //   actions.push(
+  //     <ShopExternalAction title="Abrir mapa" url={url} key="map"><Map color='primary' /></ShopExternalAction>
+  //   )
+  // }
+
+  // const onlineChip = shop && shop.online ? (
+  //   <Chip icon={<LocationOff />} label="Solo online" />
+  // ) : '';
 
   // return (
   //   <>
