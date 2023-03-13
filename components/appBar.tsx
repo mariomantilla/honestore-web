9f3e15441fe0cefa2468c44cb099431fcfba4004
Mario Mantilla <mario@solarisoffgrid.com>
2023-03-14 00:34:19 +0100

    Removed props and children of account button

--- components/appBar.tsx
+++ components/appBar.tsx
@@ -184,15 +184,15 @@ function ResponsiveAppBar() {
 							Añade tu tienda gratis
 						</Button>
 						<IconButton
-							size="large"
-							edge="end"
-							aria-label="account of current user"
-							aria-controls={menuId}
-							aria-haspopup="true"
-							//onClick={handleProfileMenuOpen}
-							color="inherit"
+							// size="large"
+							// edge="end"
+							// aria-label="account of current user"
+							// aria-controls={menuId}
+							// aria-haspopup="true"
+							// //onClick={handleProfileMenuOpen}
+							// color="inherit"
 						>
-							<AccountCircle />
+							{/* <AccountCircle /> */}
 						</IconButton>
 					</Toolbar>
 				</Container>
