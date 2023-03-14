77ff716b5df5ecdac944ef2aedc91be4b187e3ed
Mario Mantilla <mario@solarisoffgrid.com>
2023-03-14 01:30:52 +0100

    Fixed icon imports

--- components/favButton.tsx
+++ components/favButton.tsx
@@ -1,4 +1,5 @@
-import { Favorite, FavoriteBorder } from "@mui/icons-material";
+import Favorite from "@mui/icons-material/Favorite";
+import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
 import { useUser } from "@supabase/auth-helpers-react";
@@ -9,37 +10,36 @@ import Shop from "../models";
 
 export const FavButton = ({ shop, size }: { shop: Shop, size?: 'medium' | 'large' | undefined}) => {
     
-    return <></>;
-    // const user = useUser();
-    // const { userFavouriteShopsIds, addFavourite, removeFavourite } = useUserContext();
-    // const router = useRouter();
-    // const isFav = userFavouriteShopsIds.includes(shop.id);
+    const user = useUser();
+    const { userFavouriteShopsIds, addFavourite, removeFavourite } = useUserContext();
+    const router = useRouter();
+    const isFav = userFavouriteShopsIds.includes(shop.id);
 
-    // const handleFavToggle = () => {
-    //     if (!user) { 
-    //         router.push('/login');
-    //         return
-    //     }
-    //     if (isFav) {
-    //         DataService.removeFavourite(user, shop).then((r) => {if (!r.error) removeFavourite(shop.id)});
-    //     }
-    //     else {
-    //         DataService.addFavourite(user, shop).then((r) => {if (!r.error) addFavourite(shop.id)});
-    //     }
-    // }
+    const handleFavToggle = () => {
+        if (!user) { 
+            router.push('/login');
+            return
+        }
+        if (isFav) {
+            DataService.removeFavourite(user, shop).then((r) => {if (!r.error) removeFavourite(shop.id)});
+        }
+        else {
+            DataService.addFavourite(user, shop).then((r) => {if (!r.error) addFavourite(shop.id)});
+        }
+    }
 
-    // return (
-    //     <Tooltip title={isFav ? "Quitar de favoritos" : "Guardar en favoritos"}>
-    //         <IconButton
-    //             aria-label="añadir o quitar de favoritos"
-    //             onClick={() => { handleFavToggle() }}
-    //         >
-    //             {isFav ? (
-    //                 <Favorite color="primary" fontSize={size || 'medium'} />
-    //             ) : (
-    //                 <FavoriteBorder color="primary" fontSize={size || 'medium'} />
-    //             ) }            
-    //         </IconButton>
-    //     </Tooltip>
-    // );
+    return (
+        <Tooltip title={isFav ? "Quitar de favoritos" : "Guardar en favoritos"}>
+            <IconButton
+                aria-label="añadir o quitar de favoritos"
+                onClick={() => { handleFavToggle() }}
+            >
+                {isFav ? (
+                    <Favorite color="primary" fontSize={size || 'medium'} />
+                ) : (
+                    <FavoriteBorder color="primary" fontSize={size || 'medium'} />
+                ) }            
+            </IconButton>
+        </Tooltip>
+    );
 }
