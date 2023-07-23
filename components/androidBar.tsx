bb4c7e485a579dc5f455690ed2a0437880b5d8d0
Mario Mantilla <mariomantillasanchez@gmail.com>
2023-07-23 18:44:07 +0200

    Make android bar closable

--- components/androidBar.tsx
+++ components/androidBar.tsx
@@ -2,11 +2,14 @@ import Link from 'next/link'
 import Button from '@mui/material/Button'
 import Paper from '@mui/material/Paper'
 import Container from '@mui/material/Container'
+import { useEffect, useState } from 'react';
 
 
 export default function AndroidBar() {
 
-    let forceAndroid: boolean =  process.env.FORCE_ANDROID !== undefined;
+    const [open, setOpen] = useState(false);
+
+    let forceAndroid: boolean = process.env.NEXT_PUBLIC_FORCE_ANDROID !== undefined;
 
     var isMobile = {
         Android: () => navigator.userAgent.match(/Android/i) || forceAndroid,
@@ -14,12 +17,25 @@ export default function AndroidBar() {
         any: () => (isMobile.Android() || isMobile.iOS())
     };
 
+    let hide = () => {
+        localStorage.setItem('hideAndroidBar', '1');
+        setOpen(false);
+    }
+
+    useEffect(() => {
+        let hideAndroidBar = localStorage.getItem('hideAndroidBar');
+        if (!hideAndroidBar) {
+            setOpen(true);
+        }
+    })
+
     return (
-        <Paper sx={{ position: "static", top: 0, zIndex: 10, display: isMobile.Android() ? 'inherit' : 'none' }} elevation={8}>
+        <Paper sx={{ position: "static", top: 0, zIndex: 10, display: isMobile.Android() && open ? 'inherit' : 'none' }} elevation={8}>
             <Container maxWidth="lg" sx={{ padding: "15px", display: "flex", gap: {xs: 1, sm: 4}, alignItems: "center", justifyContent: "center", flexDirection: {xs: "column", sm: "row"}, textAlign: "center" }}>
-                ¿Utilizas Android? Descarga la app, ¡es aun mejor!
+                ¿Utilizas Android? !Descarga la app!
                 <Button variant="contained" href="/download" LinkComponent={Link}>Descargar</Button>
             </Container>
+            <Button onClick={hide}>X</Button>
         </Paper>
     )
 }
