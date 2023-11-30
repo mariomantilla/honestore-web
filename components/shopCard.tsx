c6d4574b042ced91205d5241c08b9ce4542c891d
Mario Mantilla <mariomantillasanchez@gmail.com>
2023-11-30 21:35:17 +0100

    Another test

--- components/shopCard.tsx
+++ components/shopCard.tsx
@@ -13,69 +13,66 @@ import { ShopLogo } from "./shopLogo";
 
 const ShopCard = ({ shop }: { shop: ShopTags | null }) => {
 
-    return null;
+	const tagsChips = shop && Array.isArray(shop.tags) ?
+		shop.tags.map((t: Tag, i: number) => (
+			<TagChip key={i} tag={t} tiny />
+		))
+	: [] ;
 
-	// const tagsChips = shop && Array.isArray(shop.tags) ?
-	// 	shop.tags.map((t: Tag, i: number) => (
-	// 		<TagChip key={i} tag={t} tiny />
-	// 	))
-	// : [] ;
+    return (
+        <Paper elevation={2} sx={{display: "flex", flexDirection: "column", height: "100%"}}>
+            <Box sx={{ display: "flex", flexDirection: "row", columnGap: 2, padding: 1.5, paddingBottom: 0 }}>
+                <Box sx={{ flexShrink: 0, display: {xs: "none", sm: "block"} }}>
+                    {shop ? (
+                        <Link href={'/shops/' + shop.slug} aria-label={`Ver más sobre el comercio "${shop.name??''}"`}>
+                            <Avatar
+                                sx={{ width: "120px", height: "120px", backgroundColor: '#fff' }}
+                                alt={shop.name ?? ''}
+                            >
+                                {/* <ShopLogo shop={shop} size={120} /> */}
+                            </Avatar>
+                        </Link>
+                    ) : (<Skeleton variant='circular' height={120} width={120} />)}
+                </Box>
+                <Box sx={{ flexShrink: 0, display: {xs: "block", sm: "none"} }}>
+                    {shop ? (
+                        <Link href={'/shops/' + shop.slug} aria-label={`Ver más sobre el comercio "${shop.name??''}"`}>
+                            <Avatar
+                                sx={{ width: "60px", height: "60px", backgroundColor: '#fff' }}
+                                alt={shop.name ?? ''}
+                            >
+                                {/* <ShopLogo shop={shop} size={60} /> */}
+                            </Avatar>
+                        </Link>
+                    ) : (<Skeleton variant='circular' height={120} width={120} />)}
+                </Box>
+                <Box sx={{ display: "flex", flexDirection: "column", rowGap: 0, flexGrow: 1, minWidth: 0 }}>
+                    <Box sx={{ display: "flex" }}>
+                        {shop ? (
+                            <Typography variant="h3" className="singleLineEllipsis" sx={{ alignSelf: "center", flexGrow: 1, fontSize: 18 }}><Link href={'/shops/' + shop.slug}>{shop.name}</Link></Typography>
 
-
-    // return (
-    //     <Paper elevation={2} sx={{display: "flex", flexDirection: "column", height: "100%"}}>
-    //         <Box sx={{ display: "flex", flexDirection: "row", columnGap: 2, padding: 1.5, paddingBottom: 0 }}>
-    //             <Box sx={{ flexShrink: 0, display: {xs: "none", sm: "block"} }}>
-    //                 {shop ? (
-    //                     <Link href={'/shops/' + shop.slug} aria-label={`Ver más sobre el comercio "${shop.name??''}"`}>
-    //                         <Avatar
-    //                             sx={{ width: "120px", height: "120px", backgroundColor: '#fff' }}
-    //                             alt={shop.name ?? ''}
-    //                         >
-    //                             <ShopLogo shop={shop} size={120} />
-    //                         </Avatar>
-    //                     </Link>
-    //                 ) : (<Skeleton variant='circular' height={120} width={120} />)}
-    //             </Box>
-    //             <Box sx={{ flexShrink: 0, display: {xs: "block", sm: "none"} }}>
-    //                 {shop ? (
-    //                     <Link href={'/shops/' + shop.slug} aria-label={`Ver más sobre el comercio "${shop.name??''}"`}>
-    //                         <Avatar
-    //                             sx={{ width: "60px", height: "60px", backgroundColor: '#fff' }}
-    //                             alt={shop.name ?? ''}
-    //                         >
-    //                             <ShopLogo shop={shop} size={60} />
-    //                         </Avatar>
-    //                     </Link>
-    //                 ) : (<Skeleton variant='circular' height={120} width={120} />)}
-    //             </Box>
-    //             <Box sx={{ display: "flex", flexDirection: "column", rowGap: 0, flexGrow: 1, minWidth: 0 }}>
-    //                 <Box sx={{ display: "flex" }}>
-    //                     {shop ? (
-    //                         <Typography variant="h3" className="singleLineEllipsis" sx={{ alignSelf: "center", flexGrow: 1, fontSize: 18 }}><Link href={'/shops/' + shop.slug}>{shop.name}</Link></Typography>
-
-    //                     ) : (<Skeleton width="60%" sx={{ alignSelf: "center", flexGrow: 1 }} />)}
-    //                     {shop ? (<FavButton shop={shop} />) : (<Skeleton width="20px" />)}
-    //                 </Box>
-    //                 <Typography variant="body2" color="text.secondary" sx={clampStyles(3)}>
-    //                     {shop ? shop.description ?? '' : (<><Skeleton width="90%" /><Skeleton width="100%" /><Skeleton width="85%" /></>)}
-    //                 </Typography>
-    //             </Box>
-    //         </Box>
-    //         <Box sx={{
-    //             display: "flex",
-    //             flexDirection: "row",
-    //             padding: 1.5,
-    //             justifyContent: "center",
-    //             gap: 1,
-    //             overflow: "hidden",
-    //             flexWrap: "wrap-reverse",
-    //             maxHeight: "3.2rem"
-    //          }}>
-    //             {tagsChips}
-    //         </Box>
-    //     </Paper>
-    // );
+                        ) : (<Skeleton width="60%" sx={{ alignSelf: "center", flexGrow: 1 }} />)}
+                        {shop ? (<FavButton shop={shop} />) : (<Skeleton width="20px" />)}
+                    </Box>
+                    <Typography variant="body2" color="text.secondary" sx={clampStyles(3)}>
+                        {shop ? shop.description ?? '' : (<><Skeleton width="90%" /><Skeleton width="100%" /><Skeleton width="85%" /></>)}
+                    </Typography>
+                </Box>
+            </Box>
+            <Box sx={{
+                display: "flex",
+                flexDirection: "row",
+                padding: 1.5,
+                justifyContent: "center",
+                gap: 1,
+                overflow: "hidden",
+                flexWrap: "wrap-reverse",
+                maxHeight: "3.2rem"
+             }}>
+                {/* {tagsChips} */}
+            </Box>
+        </Paper>
+    );
 }
 
 ShopCard.defaultProps = { shop: null, onlyFavs: false };
